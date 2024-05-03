53084d6783e570bf85faafb1428143ebd5cd6eac
Branko Bjadov <brankobjadov@gmail.com>
2024-05-03 12:06:08 +0200

    add algorithm for calculating the amount of As and Hs, use comic sans font, add image styles to wrap the scream

--- src/app/api/image/route.tsx
+++ src/app/api/image/route.tsx
@@ -1,40 +1,85 @@
 /* eslint-disable @next/next/no-img-element */
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { ImageResponse } from 'next/og';
-// App router includes @vercel/og.
-// No need to install it.
 
-export async function GET(request: NextApiRequest, response: NextApiResponse) {
+export const runtime = 'edge';
+
+const IMAGE_WIDTH = 1080;
+const NUM_OF_LINES = 15;
+const A_WIDTH = 38.5714285714;
+const H_WIDTH = 40;
+const A_MAX = 420;
+
+
+const randomInt = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const determineTextColor = (bgColor: string) => {
+  const r = parseInt(bgColor.substr(1, 2), 16);
+  const g = parseInt(bgColor.substr(3, 2), 16);
+  const b = parseInt(bgColor.substr(5, 2), 16);
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+  return brightness >= 128 ? 'black' : 'white';
+};
+
+const generateText = () => {
+  const numA = randomInt(1, A_MAX);
+  const numH = randomInt(1, Math.floor(((NUM_OF_LINES * IMAGE_WIDTH) - (A_WIDTH * numA)) / H_WIDTH));
+
+  console.log("AAAAA: ", numA);
+  console.log("HHHHH: ", numH);
+
+  console.log("number of As: ", numA);
+  console.log("max number of Hs: ", Math.floor(((NUM_OF_LINES * IMAGE_WIDTH) - (A_WIDTH * numA)) / H_WIDTH));
+
+  return "A".repeat(numA) + "H".repeat(numH);
+}
+
+export const GET = async () => {
   try {
+    const fontData = await fetch(
+      new URL('../../../../public/fonts/comicsans.ttf', import.meta.url)
+    ).then((res) => res.arrayBuffer());
+
+    const text = generateText();
+    const backgroundColor = randomHexColor();
+    const textColor = determineTextColor(backgroundColor);
     return new ImageResponse(
       (
         <span style={{
           wordWrap: 'break-word',
           height: '100%',
           width: '100%',
-          backgroundColor: '#129033',
-          fontSize: 40,
+          backgroundColor: backgroundColor,
+          fontSize: '52px',
         }}>
           <b style={{
             height: '100%',
             width: '100%',
             wordWrap: 'break-word',
-            wordBreak: 'break-all'
-          }}>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaABAsdhash</b>
+            wordBreak: 'break-all',
+            color: textColor,
+            fontFamily: '"Comic"',
+          }}>{text}</b>
         </span>
 
       ),
       {
         width: 1080,
         height: 1080,
+        fonts: [
+          {
+            name: 'Comic',
+            data: fontData,
+            style: 'normal',
+          },
+        ],
       },
     );
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (e: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    });
+  } catch (error) {
+    let message
+    if (error instanceof Error) message = error.message
+    else message = String(error)
+    return new Response(`There has been an error during the image generation.\nThe error is: ${message}`, { status: 500 });
   }
 }
