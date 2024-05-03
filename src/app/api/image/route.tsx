b7dd1392a614ab22004132d90a1beb229ef653d8
Branko Bjadov <brankobjadov@gmail.com>
2024-05-03 12:23:00 +0200

    replace deprecated method in `determineTextColor` method

--- src/app/api/image/route.tsx
+++ src/app/api/image/route.tsx
@@ -15,9 +15,9 @@ const randomInt = (min: number, max: number) => Math.floor(Math.random() * (max 
 const randomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
 const determineTextColor = (bgColor: string) => {
-  const r = parseInt(bgColor.substr(1, 2), 16);
-  const g = parseInt(bgColor.substr(3, 2), 16);
-  const b = parseInt(bgColor.substr(5, 2), 16);
+  const r = parseInt(bgColor.slice(1, 3), 16);
+  const g = parseInt(bgColor.slice(3, 5), 16);
+  const b = parseInt(bgColor.slice(5, 7), 16);
   const brightness = (r * 299 + g * 587 + b * 114) / 1000;
   return brightness >= 128 ? 'black' : 'white';
 };
