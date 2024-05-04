2cdbb807eacf122861222293f6a68153b9a57c7f
Branko Bjadov <brankobjadov@gmail.com>
2024-05-04 12:47:43 +0200

    hopefully fix qstash auth

--- src/app/api/post-to-instagram/route.ts
+++ src/app/api/post-to-instagram/route.ts
@@ -1,32 +1,23 @@
 
-import { verifySignature } from "@upstash/qstash/nextjs";
-import { type NextApiRequest, type NextApiResponse } from "next/types";
-// import { env } from "~/env";
+import { verifySignatureAppRouter } from "@upstash/qstash/dist/nextjs";
+import { type NextRequest, NextResponse } from "next/server";
 import { postImage } from "~/server/api/instagram";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+const handler = async (req: NextRequest) => {
   try {
-    // if (req.headers.authorization !== `Bearer ${env.CRON_SECRET}`) {
-    //   return new Response('Unauthorized', { status: 401 });
-    // }
     const postImageRes = await postImage();
     if (postImageRes.status === 'ok') {
-      return new Response('Posted to Instagram', { status: 200 });
+      return new NextResponse('Posted to Instagram', { status: 200 });
     } else {
-      return new Response('Failed to post to Instagram', { status: 500 });
+      return new NextResponse('Failed to post to Instagram', { status: 500 });
     }
   } catch (error) {
     let message
     if (error instanceof Error) message = error.message
     else message = String(error)
-    return new Response(`There has been an error posting to Instagram.\nThe error is: ${message}`, { status: 500 });
+    return new NextResponse(`There has been an error posting to Instagram.\nThe error is: ${message}`, { status: 500 });
   }
 }
 
-export const GET = verifySignature(handler);
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+export const GET = verifySignatureAppRouter(handler);
